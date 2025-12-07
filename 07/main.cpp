1e96f85727c9d5e30cd594907e2628997c188e18
joe <joe@email.com>
2025-12-07 20:09:19 +0000

    day 7 part2

--- 07/main.cpp
+++ 07/main.cpp
@@ -5,25 +5,28 @@
 
 void part1(const std::string &fn) {
   const auto ip = aoc::read_file(fn);
-  std::set<size_t> beams = {std::ranges::find(ip.at(0), 'S') -
-                            ip.at(0).begin()};
+  std::map<size_t, size_t> beams = {
+      {std::ranges::find(ip.at(0), 'S') - ip.at(0).begin(), 1UL}};
   std::println("{}", beams);
 
   size_t nsplit{};
   for (const std::string &line : ip | std::views::drop(1)) {
-    std::set<size_t> new_beams;
-    for (size_t ibeam : beams) {
+    std::map<size_t, size_t> new_beams;
+    for (auto [ibeam, nhist] : beams) {
       if (line.at(ibeam) == '^') {
         nsplit++;
-        new_beams.insert(ibeam + 1);
-        new_beams.insert(ibeam - 1);
+        new_beams[ibeam + 1] += nhist;
+        new_beams[ibeam - 1] += nhist;
       } else {
-        new_beams.insert(ibeam);
+        new_beams[ibeam] += nhist;
       }
     }
     beams = new_beams;
   }
+  const auto ntimeline =
+      std::ranges::fold_left(beams | std::views::values, 0UL, std::plus());
   std::println("day 07 part 1 = {} ({})", nsplit, fn);
+  std::println("day 07 part 2 = {} ({})", ntimeline, fn);
 }
 
 int main() {
