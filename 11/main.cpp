6a8f2492e3fc1d1b060ea783029af65f6fac0617
joe <joe@email.com>
2025-12-11 22:23:16 +0000

    day 11 part 2

--- 11/main.cpp
+++ 11/main.cpp
@@ -24,7 +24,7 @@ auto paths(const std::map<std::string, std::set<std::string>> &graph,
 void part1(const std::string &fn) {
   const auto ip = aoc::read_file(fn) | std::views::transform(parse) |
                   std::ranges::to<std::map>();
-  //std::println("{}", ip);
+  // std::println("{}", ip);
   std::set<std::string> all;
   for (const auto &[k, v] : ip) {
     all.insert(k);
@@ -35,7 +35,58 @@ void part1(const std::string &fn) {
   std::println("day 11 part 1 = {}", ans);
 }
 
+size_t paths2(std::map<std::string, size_t> &cache,
+              const std::map<std::string, std::set<std::string>> &graph,
+              std::set<std::string> visited, const std::string &target,
+              const std::string &s) {
+  if (cache.contains(s)) {
+    // std::println("using cache!");
+    return cache.at(s);
+  }
+  // std::println("{}", s);
+  size_t result{};
+  if (!visited.contains(s)) {
+    visited.insert(s);
+    if (s == target) {
+      result = 1UL;
+    } else if (graph.contains(s)) {
+      for (const std::string &s2 : graph.at(s)) {
+        result += paths2(cache, graph, visited, target, s2);
+      }
+    }
+  }
+  cache.insert({s, result});
+  // std::println("{}", cache);
+  return result;
+}
+
+void part2(const std::string &fn) {
+  const auto ip = aoc::read_file(fn) | std::views::transform(parse) |
+                  std::ranges::to<std::map>();
+  // std::println("{}", ip);
+  std::set<std::string> all;
+  for (const auto &[k, v] : ip) {
+    all.insert(k);
+    for (const auto &s : v)
+      all.insert(s);
+  }
+  auto comb = [&](const std::string &a, const std::string &b) {
+    std::map<std::string, size_t> cache;
+    return paths2(cache, ip, std::set<std::string>{}, b, a);
+  };
+  const auto svr_fft = comb("svr", "fft");
+  const auto fft_dac = comb("fft", "dac");
+  const auto dac_out = comb("dac", "out");
+  const auto svr_dac = comb("svr", "dac");
+  const auto dac_fft = comb("dac", "fft");
+  const auto fft_out = comb("fft", "out");
+  std::println("day 11 part 2 = {}",
+               svr_fft * fft_dac * dac_out + svr_dac * dac_fft * fft_out);
+}
+
 int main() {
   part1("test_input");
   part1("input");
+  part2("test_input2");
+  part2("input");
 }
